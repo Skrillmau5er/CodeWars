c23b525830805f9bda99245358a0fdbc51064ffd
Alec Taylor Ostheimer <coman3@et.byu.edu>
2019-02-14 09:07:55 -0700

    making sure changes are pushed

--- JavaScript/Simple-Encryption/encrypt.js
+++ JavaScript/Simple-Encryption/encrypt.js
@@ -16,47 +16,39 @@
 //If n is <= 0 then return the input text.
 
 function encrypt(text, n) {
-
-    for(var a = 0; a < n; a++){
-        evenarr = [];
-        oddarr = [];
-        for(var i = 0; i < text.length; i++){
-            (i%2) ? evenarr.push(text[i]) : oddarr.push(text[i]);
+    if(text == null) return null;
+    if(n > 0){
+        for(var a = 0; a < n; a++){
+            evenarr = [];
+            oddarr = [];
+            for(var i = 0; i < text.length; i++){
+                (i%2) ? evenarr.push(text[i]) : oddarr.push(text[i]);
+            }
+            text = ((evenarr.join('')) + (oddarr.join('')));
         }
-        text = ((evenarr.join('')) + (oddarr.join('')));
     }
     return text;
 }
 
 function decrypt(encryptedText, n) {
-    var Elen = encryptedText.length;
-    var text = "";
-    for(var a = 0; a < n; a++){
-        evenarr = [];
-        oddarr = [];
-        for(var i = 0; i < Elen; i++){
-            if(i < Math.floor(Elen/2)){
-                console.log(encryptedText[i]);
-                evenarr.push(encryptedText[i]);
+    if(encryptedText == null) return null;
+
+    if(n > 0){
+        var Elen = encryptedText.length;
+        for(var a = 0; a < n; a++){
+            evenarr = [];
+            oddarr = [];
+            var text = "";
+            for(var i = 0; i < Elen; i++){
+                (i < Math.floor(Elen/2)) ? evenarr.push(encryptedText[i]) : oddarr.push(encryptedText[i]);
             }
-            else{
-                console.log(encryptedText[i]);
-                oddarr.push(encryptedText[i]);
+            for(var i = 0; i < Elen; i++){
+                (i%2) ? (text += evenarr.shift()) : text += oddarr.shift();
             }
+            encryptedText = text;
         }
-        for(var i = 0; i < Elen; i++){
-            if(i%2){
-                text += evenarr.shift();
-                console.log(text);
-            }
-            else{
-                text += oddarr.shift();
-                console.log(text);
-            }
-        }
-        encryptedText = text;
     }
-    return text;
+    return encryptedText;
 }
 
-decrypt("s eT ashi tist!",2);
+console.log(decrypt("s eT ashi tist!",2));
