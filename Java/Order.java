f3115f7850b26a38e28041d57b90888b5cc93088
Alec Taylor Ostheimer <coman3@et.byu.edu>
2019-02-15 16:45:42 -0700

    Started Java Challenge

---
+++ Java/Order.java
@@ -0,0 +1,25 @@
+/*Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""*/
+import java.util.*;
+public class Order {
+
+  public static void main(String args[]){
+    order("4of Fo1r pe6ople g3ood th5e the2");
+  }
+  public static String order(String words) {
+    Dictionary order = new Hashtable<Integer, String>();
+    String[] wordsOrd = words.split(" ");
+    for(int i = 0; i < wordsOrd.length; i++){
+      order.put(i, wordsOrd[i]);
+    }
+    return words;
+  }
+}
